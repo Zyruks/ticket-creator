0b2bfedb4c1cc59eeee53fcce0c891957209a283
Samuel Llibre-Pillco <dev@zyruks.com>
2026-01-04 21:26:18 -0400

    refactor: enhance component styles and structure for improved readability and consistency across UI components

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -6,8 +6,8 @@ function Card({ className, ...props }: React.ComponentProps<'div'>) {
 		<div
 			data-slot="card"
 			className={cn(
-				'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm',
-				className
+				'flex flex-col gap-6 rounded-xl border bg-card py-6 text-card-foreground shadow-sm',
+				className,
 			)}
 			{...props}
 		/>
@@ -20,7 +20,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
 			data-slot="card-header"
 			className={cn(
 				'@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-2 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6',
-				className
+				className,
 			)}
 			{...props}
 		/>
@@ -31,7 +31,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
 	return (
 		<div
 			data-slot="card-title"
-			className={cn('leading-none font-semibold', className)}
+			className={cn('font-semibold leading-none', className)}
 			{...props}
 		/>
 	);
@@ -58,7 +58,13 @@ function CardAction({ className, ...props }: React.ComponentProps<'div'>) {
 }
 
 function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
-	return <div data-slot="card-content" className={cn('px-6', className)} {...props} />;
+	return (
+		<div
+			data-slot="card-content"
+			className={cn('px-6', className)}
+			{...props}
+		/>
+	);
 }
 
 function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
